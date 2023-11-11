bdf7596c16d28fbbeab21551777e06a25c960faf
JunmingYi <120333740+Junmingyi0112@users.noreply.github.com>
2023-11-11 04:10:40 -0600

    new file:   .DS_Store 	new file:   .Rhistory 	modified:   historic_property_data.csv 	modified:   model.r 	modified:   predict_property_data.csv

--- model.r
+++ model.r
@@ -3,8 +3,39 @@ library(dplyr)
 library(caret)
 library(ggplot2)
 library(rpart)
-# Load the data files
-predict_data <- read.csv("predict_property_data.csv")
-historic_data <- read.csv("historic_property_data.csv")
-codebook <- read.csv("codebook.csv")
+library(leaps)
+library(psych)
 
+# Load the data files
+training <- read.csv('https://raw.githubusercontent.com/Junmingyi0112/FIN550-FA23-G7/main/historic_property_data.csv')
+test <- read.csv('https://raw.githubusercontent.com/Junmingyi0112/FIN550-FA23-G7/main/predict_property_data.csv')
+head(training)
+head(test)
+
+# EDA
+# turn chr to factor in a loop
+for (i in 1:ncol(training)) {
+  if (class(training[,i]) == "character") {
+    training[,i] <- as.factor(training[,i])
+  }
+}
+# turn ture or false values into 1 or 0 for all columns
+for (i in 1:ncol(training)) {
+  if (class(training[,i]) == "logical") {
+    training[,i] <- as.numeric(training[,i])
+  }
+}
+# drop variables with only has one unique value
+training <- training[, sapply(training, function(x) length(unique(x))) > 1]
+# Check the data structure again
+str(training)
+
+
+# exhaustive search
+search <- regsubsets(sale_price ~ ., data = training, nbest = 2, nvmax = 12, method = "exhaustive")
+# summary table 
+sum<-summary(search)
+# show models
+sum$which 
+# show metrics
+sum$adjr2
